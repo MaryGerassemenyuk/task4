a45101d89b66630101d159cfc4c3d415e27dc3fc
MaryGerassemenyuk <masha.1735@gmail.com>
2021-11-29 18:03:44 +0300

    Добавьте файлы проекта.

---
+++ task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            string sr = File.ReadAllText(path);
+            string[] separators = { " ", "\r\n" };
+            string[] mass = sr.Split(separators, StringSplitOptions.None);
+            int[] arr = Array.ConvertAll(mass, int.Parse);
+            Array.Sort(arr);
+            int n = arr.Length / 2;
+            int median = arr[n - 1];
+            int sum = 0;
+            foreach (int elem in arr)
+
+            {
+                int mod = Math.Abs(elem - n);
+                sum = sum + mod;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
